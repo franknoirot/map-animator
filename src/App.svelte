d69769869d7478845717a2e75bc4f7987cb62270
Frank Johnson <Frank@Franks-Macbook.local>
2020-01-03 12:43:14 -0500

    cleaned up UI code to be more declarative

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,8 @@
 	import ColorPicker from './components/ColorPicker.svelte'
 	import { geoBaseData as dotPromise } from './zipToGeo.js'
 	
+	let showDots = false
+
 	let animParams = {
 			animLength: 6,
 			animSpread: 0.2,
@@ -10,13 +12,26 @@
 			maxGrowthFactor: 1.5,
 			growthDuration: .2,
 		}
-	let showDots = false
-	let colors = {
+	const rangeUI = [
+		{param: 'animLength', label: 'Duration', labelSuffix: ' s', min: 1, max: 10, step: 0.1,},
+		{param: 'animSpread', label: 'Spread', labelSuffix: '', min: 0, max: 1, step: 0.01,},
+		{param: 'dotSize', label: 'Base Dot Size', labelSuffix: '', min: 1, max: 7, step: 0.1,},
+		{param: 'maxGrowthFactor', label: 'Max Dot Size', labelSuffix: 'x', min: 1.1, max: 4.5, step: 0.01,},
+		{param: 'growthDuration', label: 'Growth Duration', labelSuffix: ' s', min: .05, max: .5, step: 0.01,},
+	]
+
+	const colors = {
 		dotColor: {r: 252, g: 193, b: 59},
 		bgColor: {r: 75, g:112, b:179},
 		stateColor: {r: 2, g: 16, b: 54},
 		textColor: {r: 234, g: 245, b:255},
 	}
+	const colorUI = [
+		{param: 'dotColor', label: 'Dot Color'},
+		{param: 'bgColor', label: 'Background Color'},
+		{param: 'stateColor', label: 'State Color'},
+		{param: 'textColor', label: 'Text Color'},
+	]
 
 	// dotPromise.then(data => console.log('data fetched! ', data)) // For debugging
 </script>
@@ -25,40 +40,22 @@
 	{...animParams}	{...colors}></Map>
 <section>
 	<button class='show-dots' on:click={() => showDots = !showDots}>{(showDots) ? 'Hide' : 'Show'} Dots</button>
-	<label class='range-group'>Duration
-		<input type="range" min='1' max='10' step="0.1" value={ animParams.animLength } on:input={e => animParams.animLength = e.target.value}>
-		{animParams.animLength} seconds
+	{#each rangeUI as range, i (i)}
+	<label class='range-group'>{ range.label }
+		<input type="range" min={range.min} max={range.max} step={range.step} value={ animParams[range.param] }
+		on:input={e => animParams[range.param] = e.target.value}>
+		{animParams[range.param] + range.labelSuffix}
 	</label>
-	<label class='range-group'>Spread
-		<input type="range" min='0' max='1' step="0.01" value={ animParams.animSpread } on:input={e => animParams.animSpread = e.target.value}>
-		{animParams.animSpread}
-	</label>
-	<label class='range-group'>Base Dot Size
-		<input type="range" min='1' max='7' step="0.1" value={ animParams.dotSize } on:input={e => animParams.dotSize = e.target.value}>
-		{animParams.dotSize}
-	</label>
-	<label class='range-group'>Max Dot Size
-		<input type="range" min='1.1' max='4.5' step="0.01" value={ animParams.maxGrowthFactor } on:input={e => animParams.maxGrowthFactor = e.target.value}>
-		{animParams.maxGrowthFactor}
-	</label>
-	<label class='range-group'>Growth Duration
-		<input type="range" min='.05' max='.5' step="0.01" value={ animParams.growthDuration } on:input={e => animParams.growthDuration = e.target.value}>
-		{animParams.growthDuration}
-	</label>
+	{/each}
 </section>
 <section>
-	<ColorPicker bind:rgb={colors.dotColor}>
-		<p>Dot Color</p>
-	</ColorPicker>
-	<ColorPicker bind:rgb={colors.bgColor}>
-		<p>Background Color</p>
-	</ColorPicker>
-	<ColorPicker bind:rgb={colors.stateColor}>
-		<p>State Color</p>
-	</ColorPicker>
-	<ColorPicker bind:rgb={colors.textColor}>
-		<p>Text Color</p>
-</ColorPicker>
+	{#each colorUI as picker, i (i)}
+	<div>
+		<ColorPicker bind:rgb={ colors[picker.param] }>
+			<p>{ picker.label }</p>
+		</ColorPicker>
+	</div>
+	{/each}
 </section>
 
 <style>
@@ -66,9 +63,9 @@
 		margin: 2em 0;
 		display: grid;
 		grid-template-columns: 50% 50%;
-		grid-gap: 2em;
+		grid-gap: 1em 1.5em;
 		width: 95vw;
-		max-width: 600px;
+		max-width: 660px;
 	}
 
 	.show-dots {
@@ -77,6 +74,7 @@
 
 	.range-group {
 		display: block;
+		width: max-content;
 		margin: 1em 0;
 	}
 </style>
